c8287303e7e79d61270168c757fc0897dafb99ff
Kyle Esurena <kyleesurena1@gmail.com>
2026-01-07 22:10:41 +0800

    Added mode toggle

---
+++ src/components/landing-page/NavBar.tsx
@@ -0,0 +1,8 @@
+import { ModeToggle } from "../layout/ModeToggle";
+
+
+export default function NavBar () {
+    return (
+        <ModeToggle/>
+    )
+}
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,23 +1,9 @@
-import { Button } from "@/components/ui/button"
+//import { Button } from "@/components/ui/button"
+import  NavBar from "@/components/landing-page/NavBar"
 export default function LandingPage(){
     return (
         <>
-        <div>
-    <div className="flex flex-col items-center gap-4 p-10">
-      {/* Default variant */}
-      <Button>Default Button</Button>
-
-      {/* Destructive variant */}
-      <Button variant="destructive">Delete</Button>
-
-      {/* Outline variant */}
-      <Button variant="outline">Secondary Action</Button>
-
-      {/* Sizes */}
-      <Button size="sm">Small Button</Button>
-      <Button size="lg">Large Button</Button>
-    </div>
-        </div>
+            <NavBar/>
         </>
     )
 }
