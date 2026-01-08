f78eabb37f36225c159e7c2f4a15058ec16387fe
Kyle Esurena <kyleesurena1@gmail.com>
2026-01-08 23:13:06 +0800

    Header done

--- src/components/landing-page/NavBar.tsx
+++ src/components/landing-page/NavBar.tsx
@@ -1,8 +1,35 @@
 import { ModeToggle } from "../layout/ModeToggle";
-
+import logo from "../../assets/code.png";
 
 export default function NavBar () {
     return (
-        <ModeToggle/>
+        <header className="bg-background">
+            <div className="flex justify-between items-center px-4 py-2 font-sans">
+
+                {/* logo */}
+                <div className="flex items-center space-x-2">
+                    <img src={logo} alt="Logo" className="h-10 w-auto" />
+                    <div className="text-xl font-bold">I'm trying to learn man!</div>
+                </div>
+
+                {/* navigation-menu */}
+                <nav className="space-x-6 text-base font-medium ">
+                    <a href="#" className="text-muted-foreground hover:text-foreground ">Data Structures</a>
+                    <a href="#" className="text-muted-foreground hover:text-foreground">Algorithms</a>
+                    <a href="#" className="text-muted-foreground hover:text-foreground">Fundamentals</a>
+                </nav>
+
+                {/* theme-toggle */}
+                <ModeToggle/>
+            </div>
+
+
+        </header>
+
+
+
+
+
+
     )
 }
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -3,7 +3,9 @@ import  NavBar from "@/components/landing-page/NavBar"
 export default function LandingPage(){
     return (
         <>
+        <div className="w-full h-screen">
             <NavBar/>
+        </div>
         </>
     )
 }
