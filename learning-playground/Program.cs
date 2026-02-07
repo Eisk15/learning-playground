57de17661a57d24e8d43b06ecdfba2dd06cae7f9
Kyle Esurena <kyleesurena1@gmail.com>
2026-02-07 22:47:28 +0800

    bank console

--- learning-playground/Program.cs
+++ learning-playground/Program.cs
@@ -1,2 +1,99 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using learning_playground.BankAccount;
+
+
+
+
+string line = "-----------------------";
+BankAccount user;
+
+while (true)
+{
+    Console.WriteLine(line);
+    Console.Write("Enter amount to create account: ");
+
+    if (double.TryParse(Console.ReadLine(), out double initialAmount)){
+        user = new BankAccount(initialAmount);
+        Console.WriteLine($"Successfully created account with {initialAmount} initial amount");
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Error try again!");
+    }
+}
+
+
+
+
+while (true)
+{
+    Console.WriteLine(line);
+    Console.Write("Action:\n" +
+        "[1] Check Balance\n" +
+        "[2] Deposit\n" +
+        "[3] Withdraw\n" +
+        "[0] Exit: ");
+    string choice = Console.ReadLine();
+    switch (choice)
+    {
+        case "1":
+            Console.WriteLine(line);
+            Console.WriteLine($"Balance: {user.Amount}");
+            continue;
+        case "2":
+            Console.WriteLine(line);
+            Console.Write("Enter amount to Deposit:");
+            if (double.TryParse(Console.ReadLine(), out double depositMoney))
+            {
+                if (user.Deposit(depositMoney))
+                {
+                    Console.WriteLine(line);
+                    Console.WriteLine($"Successfully deposit {depositMoney}\n" +
+                        $"New Balance: {user.Amount}");
+                    continue;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+            else
+            {
+                continue;
+            }
+
+        case "3":
+
+            Console.WriteLine(line);
+            Console.Write("Enter amount to Deposit:");
+            if (double.TryParse(Console.ReadLine(), out double withdrawMoney))
+            {
+                if (user.Withdraw(withdrawMoney))
+                {
+                    Console.WriteLine(line);
+                    Console.WriteLine($"Successfully withdrawn {withdrawMoney}\n" +
+                        $"New Balance: {user.Amount}");
+                    continue;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+            else
+            {
+                continue;
+            }
+
+        case "0":
+            break;
+        default:
+            Console.WriteLine(line);
+            Console.WriteLine("Try again: enter 1-4");
+            Console.WriteLine(line);
+            continue;
+    }
+
+
+
+}
---
+++ learning-playground/bank/bank.cs
@@ -0,0 +1,52 @@
+﻿namespace learning_playground.BankAccount
+{
+    class BankAccount
+    {
+        public double Amount { get; set; }
+
+
+        public BankAccount(double money)
+        {
+            if (money < 2000)
+            {
+                throw new Exception("Minimum P2000 required!");
+            }
+            else
+            {
+                Amount = money;
+            }
+        }
+
+
+        public bool Deposit(double money)
+        {
+            if (money <= 0)
+            {
+                return false;
+            }
+            else
+            {
+                Amount += money;
+                return true;
+            }
+        }
+
+        public bool Withdraw(double money)
+        {
+            if (money > Amount || money <= 0)
+            {
+                return false;
+            }
+            else
+            {
+                Amount -= money;
+                return true;
+            }
+        }
+    }
+
+}
+
+
+
+    
